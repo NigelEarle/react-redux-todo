a9300aa818fd52f539308a836cab033ee8692310
nwre87 <nwre87@gmail.com>
2017-05-27 11:05:29 -1000

    webpack prod config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -6,12 +6,61 @@ const baseConfig = require('./webpack.config.base');
 
 const prodConfig = Object.assign({}, baseConfig);
 
-prodConfig.entry = [path.join(__dirname, 'client/app.js')];
+prodConfig.entry.push(path.join(__dirname, 'client/app.js'));
 
 prodConfig.output = {
   path: path.join(__dirname, '/build/'),
   publicPath: '/',
   filename: 'bundle.js',
 };
+
+prodConfig.modules.rules.push(
+  {
+    test: /\.s?css$/,
+    exclude: /node_modules/,
+    loader: ExtractTextPlugin.extract({
+      fallbackLoader: 'style-loader',
+      loader: [
+        'css-loader',
+        'sass-loader',
+        'postcss-loader',
+      ],
+    }),
+  }
+);
+
+prodConfig.plugins.push(
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(true),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+  }),
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'client', 'index.ejs'),
+    inject: 'body',
+    filename: 'index.html',
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      keepClosingSlash: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    },
+  }),
+  new ExtractTextPlugin({
+    filename: 'style.css',
+    allChunks: true,
+  }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  })
+);
 
 module.exports = prodConfig;
